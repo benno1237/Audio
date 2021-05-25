4b51a425aa8cb4039c1b9e8ba72cb5d79452ab10
Draper <27962761+Drapersniper@users.noreply.github.com>
2021-05-25 13:18:16 +0100

    bump

--- audio/core/cog_utils.py
+++ audio/core/cog_utils.py
@@ -10,7 +10,7 @@ from redbot.core.i18n import Translator
 
 from ..converters import get_lazy_converter, get_playlist_converter
 
-__version__ = VersionInfo.from_json({"major": 3, "minor": 0, "micro": 0, "releaselevel": "alpha"})
+__version__ = VersionInfo.from_json({"major": 3, "minor": 0, "micro": 0, "releaselevel": "alpha", "serial": 1})
 
 __author__ = ["aikaterna", "Draper"]
 
