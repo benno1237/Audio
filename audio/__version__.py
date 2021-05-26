76e037c655268e8e49f7bfef62dcd8a67a90495b
Draper <27962761+Drapersniper@users.noreply.github.com>
2021-05-26 22:45:05 +0100

    not the cleanest method possible ... that being said it appends the last commit to a file so it is shown on the cog version automagically with pre-commit

--- audio/__version__.py
+++ audio/__version__.py
@@ -1,6 +1,8 @@
 from redbot import VersionInfo
 
-COMMIT = "c45211955"  # TODO: UPDATE ME
+from .hash import version
+
+COMMIT = version
 
 VERSION = VersionInfo.from_json(
     {"major": 3, "minor": 0, "micro": 0, "releaselevel": "alpha", "serial": 2}
---
+++ audio/hash.py
@@ -0,0 +1,1 @@
+version = "bc88858da"
