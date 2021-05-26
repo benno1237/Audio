c4521195503c309f74ce93700b462383204b2b4a
Draper <27962761+Drapersniper@users.noreply.github.com>
2021-05-26 19:49:50 +0100

    fix tests

--- audio/__version__.py
+++ audio/__version__.py
@@ -1,6 +1,6 @@
 from redbot import VersionInfo
 
-COMMIT = "c609cc85a"  # TODO: UPDATE ME
+COMMIT = "176c30474"  # TODO: UPDATE ME
 
 VERSION = VersionInfo.from_json(
     {"major": 3, "minor": 0, "micro": 0, "releaselevel": "alpha", "serial": 2}
